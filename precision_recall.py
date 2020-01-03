2663501dd0964262abfb2d3114f75029bc71d019
Albert Leśniak <albert.lesniak@ijp.pan.pl>
2020-01-03 14:55:10 +0100

    precision and recall completed

--- precision_recall.py
+++ precision_recall.py
@@ -1,4 +1,6 @@
+import re
 import os
+import pandas as pd
 from bs4 import BeautifulSoup
 from distutils.util import strtobool
 
@@ -35,14 +37,22 @@ def calculate_metrics(tp, tn, fp, fn, N):
     f1 = 2 * (precision * recall) / (precision + recall)
     return precision, recall, accuracy, f1
 
+def find_baseline(ann_file, baseline_path):
+    lang = re.search('annotated_(\w{3})\.xml', ann_file).group(1)
+    if lang in os.listdir(baseline_path):
+        lang_path = os.path.join(baseline_path, lang)
+        base_xml = os.path.join(lang_path, os.listdir(lang_path)[0])
+        return lang, base_xml
+
 if __name__ == '__main__':
-    man_annotated_path = os.path.abspath('samples_man_annotated/joanna/annotated_deu/annotated_base_deu1.xml')
-    man_annotated_paragraphs = extract_paragraphs(man_annotated_path)
-    baseline_path = os.path.abspath('samples_baseline/deu/base_deu1.xml')
-    baseline_paragraphs = extract_paragraphs(baseline_path)
-    tp, tn, fp, fn, N = count_values(true_elements=man_annotated_paragraphs, predicted_elements=baseline_paragraphs)
-    precision, recall, accuracy, f1 = calculate_metrics(tp=tp, tn=tn, fp=fp, fn=fn, N=N)
-    print('Precision:', precision)
-    print('Recall:', recall)
-    print('Accuracy:', accuracy)
-    print('F1:', f1)
+    annotated_path = os.path.abspath('new/samples_man_annotated')
+    baseline_path = os.path.abspath('new/samples_baseline')
+    df = pd.DataFrame(columns=['precision', 'recall', 'accuracy', 'f1'])
+    for ann_file in os.listdir(annotated_path):
+        print(f'Processing: {ann_file}')
+        lang, base_file = find_baseline(ann_file=ann_file, baseline_path=baseline_path)
+        baseline_paragraphs = extract_paragraphs(xml_path=base_file)
+        annotated_paragraphs = extract_paragraphs(os.path.join(annotated_path, ann_file))
+        tp, tn, fp, fn, N = count_values(true_elements=annotated_paragraphs, predicted_elements=baseline_paragraphs)
+        df.loc[lang] = calculate_metrics(tp=tp, tn=tn, fp=fp, fn=fn, N=N)
+    df.to_excel('precision_recall_output.xlsx')
